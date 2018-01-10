9a1e3a249df68fad9927abce575e91c7b9b0507a
ngdong <2559635030@qq.com>
2018-01-10 16:16:32 +0800

    s

--- src/android/SystemActivity.java
+++ src/android/SystemActivity.java
@@ -6,11 +6,18 @@ import android.os.Build;
 import android.provider.Settings;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 public class SystemActivity extends CordovaPlugin {
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
